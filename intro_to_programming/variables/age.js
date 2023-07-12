4e702691c6c2a00043452c8d627cabb4cdb37a0c
Zach Porter <znporter24@gmail.com>
2023-07-12 13:28:49 -0700

    update age.js solution

--- intro_to_programming/variables/age.js
+++ intro_to_programming/variables/age.js
@@ -13,7 +13,7 @@ In 40 years, you will be 60 years old.
 let rlSync = require('readline-sync');
 let age = Number(rlSync.question('What is your age? '));
 console.log(`You are ${age} years old`);
-console.log(`In 10 years, you will be ${age + 10} years old.`);
-console.log(`In 20 years, you will be ${age + 20} years old.`);
-console.log(`In 30 years, you will be ${age + 30} years old.`);
-console.log(`In 40 years, you will be ${age + 40} years old.`);
+
+for (let i = 10; i <= 40; i += 10) {
+  console.log(`In ${i} years, you will be ${age + i} years old.`);
+}
